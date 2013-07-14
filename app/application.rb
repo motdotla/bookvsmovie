558139cdec8a2deeea9718040c1b0356d4738815
scottmotte <scott@scottmotte.com>
2013-07-13 21:15:11 -0700

    Shows the images in the js

--- app/application.rb
+++ app/application.rb
@@ -14,33 +14,40 @@ class Application < Sinatra::Base
     File.read(File.join('public', 'stylesheets', params[:path]))
   end
 
-  get "/javascripts/:path" do
+  get "/javascripts/jquery.js" do
     content_type 'application/javascript'
-    File.read(File.join('public', 'javascripts', params[:path]))
+    File.read(File.join('public', 'javascripts', "jquery.js"))
   end
 
   get "/search.json" do
     content_type 'application/json'
-
-    title   = params[:q]
     
-    # Book
-    client  = Goodreads.new
-    book    = client.book_by_title title
+    begin
+      title   = params[:q]
+      
+      # Book
+      client  = Goodreads.new
+      book    = client.book_by_title title
 
-    # Movie
-    result  = ROTTENTOMATOES.get "/api/public/v1.0/movies.json?apikey=#{ROTTENTOMATOES_API_KEY}&q=#{title}&page_limit=1"
-    json    = JSON.parse(result.body)
-    movie   = json["movies"][0]
-    
-    {
-      success: true,
-      book: {
-        image_url: book["image_url"]
-      },
-      movie: {
-        image_url: movie["posters"]["profile"]
-      }
-    }.to_json
+      # Movie
+      result  = ROTTENTOMATOES.get "/api/public/v1.0/movies.json?apikey=#{ROTTENTOMATOES_API_KEY}&q=#{title}&page_limit=1"
+      json    = JSON.parse(result.body)
+      movie   = json["movies"][0]
+      
+      {
+        success: true,
+        book: {
+          image_url: book["image_url"]
+        },
+        movie: {
+          image_url: movie["posters"]["profile"]
+        }
+      }.to_json
+    rescue
+      {
+        success: false,
+        error: "There was an error. Try again"
+      }.to_json
+    end
   end
 end
